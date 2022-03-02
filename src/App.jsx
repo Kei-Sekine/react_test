2bb9129f60ce4e3f49d7a1044fc4d670edb8dbc7
Kei-Sekine <100555006+Kei-Sekine@users.noreply.github.com>
2022-03-02 12:22:04 +0900

    React React

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickSwtchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>TEST</h1>
-      <p style={contentStyle}>aaa</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue" message="おげんきですか？" />
+      <ColorfulMessage color="pink" message="病気です" />
+      <button onClick={onClickCountUp}>ボタン</button>
+      <br />
+      <button onClick={onClickSwtchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(aaaa)</p>}
     </>
   );
 };
